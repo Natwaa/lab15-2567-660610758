970e2819ac05e1f57b50efb0de01c27abef65378
Natwaa <Natwara_ch@cmu.ac.th>
2024-09-10 16:25:22 +0700

    finishhh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,18 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
-  );
+  )
+  .refine(
+    (data) => {
+      if (data.password === data.confirmPassword) return true;
+      return false;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    },
+  )
+  ;
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -96,10 +107,14 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
+    if(form.values.coupon === "CMU2023") price = price*0.7;
 
     return price;
   };
@@ -187,7 +202,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer year={2024} fullName="Natwara Chaiyasit" studentId="660610758" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
